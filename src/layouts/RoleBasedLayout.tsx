8a3e0be5ef095082b2637b285b0a20ea93b46420
NuraliAKH <axmadovnurali5@gmail.com>
2025-07-27 02:06:35 +0500

    role based layout

---
+++ src/layouts/RoleBasedLayout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useUserContext } from "../contexts/UserContext";
+import UserLayout from "./UserLayout";
+import GymOwnerLayout from "./GymOwnerLayout";
+import { useUserProfile } from "../features/user/profile/hooks/useUserProfile";
+
+export const RoleBasedLayout = () => {
+  const { data } = useUserProfile();
+  console.log(data);
+
+  switch (data?.data.role) {
+    case "USER":
+      return <UserLayout />;
+    case "GYM_OWNER":
+      return <GymOwnerLayout />;
+    case "ADMIN":
+      return <GymOwnerLayout />;
+    default:
+      return <UserLayout />; // fallback
+  }
+};
